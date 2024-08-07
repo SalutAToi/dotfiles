03d5e54ac19420369002c34a1daff9dfdb7bf5aa
SalutAToi <5493696+SalutAToi@users.noreply.github.com>
2024-08-07 10:04:14 +0200

    lazyvim config

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+
+    opts = function()
+      -- PERF: we don't need this lualine require madness 🤷
+      local lualine_require = require("lualine_require")
+      lualine_require.require = require
+
+      local icons = require("lazyvim.config").icons
+
+      vim.o.laststatus = vim.g.lualine_laststatus
+      local opts = {
+        sections = {
+          lualine_c = {
+            LazyVim.lualine.root_dir(),
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            { LazyVim.lualine.pretty_path({
+              length = 4,
+            }) },
+          },
+        },
+      }
+      return opts
+    end,
+  },
+}
